1a43cb1bf45d64d28329ed60548813fa00fc8c91
Macjuul <minecraftmacjuul@gmail.com>
2015-11-28 19:41:42 +0100

    Updated to v2.2.2

--- src/me/macjuul/chexodius/CHExodiusMain.java
+++ src/me/macjuul/chexodius/CHExodiusMain.java
@@ -13,7 +13,7 @@ extends AbstractExtension {
     public static CHExodiusEventListener listener;
 
     public Version getVersion() {
-        return new SimpleVersion(2, 2, 0);
+        return new SimpleVersion(2, 2, 2);
     }
 
     public void onShutdown() {
