5fb59b9d44a2b2d494f6c9a2b7a7e1bbd3b6851d
Macjuul <minecraftmacjuul@gmail.com>
2016-02-16 19:11:11 +0100

    Fixed/added Util.getLocation(CArray location)

--- src/me/macjuul/chexodius/UtilClass.java
+++ src/me/macjuul/chexodius/UtilClass.java
@@ -4,8 +4,12 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+
+import com.laytonsmith.core.constructs.CArray;
+import com.laytonsmith.core.constructs.Target;
 
 public class UtilClass {
 
@@ -39,5 +43,15 @@ public class UtilClass {
         }
 
         return null;
+    }
+    
+    public static Location getLocation(CArray location) {
+        Target t = Target.UNKNOWN;
+        int x = Integer.valueOf(location.get(0, t).val());
+        int y = Integer.valueOf(location.get(1, t).val());
+        int z = Integer.valueOf(location.get(2, t).val());
+        World world = Bukkit.getWorld(location.get(3, t).val());
+        
+        return new Location(world, x, y, z);
     }
 }
