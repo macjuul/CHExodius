90cbb27a3b38976ec4faf49cfa98f610ec85d41a
macjuul <minecraftmacjuul@gmail.com>
2016-06-18 20:09:03 +0200

    Changed stuff

--- src/me/macjuul/chexodius/events/EventListeners.java
+++ src/me/macjuul/chexodius/events/EventListeners.java
@@ -1,18 +1,17 @@
-package me.macjuul.chexodius.abstraction.event;
+package me.macjuul.chexodius.events;
 
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityToggleGlideEvent;
-import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.event.entity.EntityCombustEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 
 import com.laytonsmith.abstraction.events.MCProjectileHitEvent;
 import com.laytonsmith.annotations.event;
 import com.laytonsmith.core.events.Driver;
 import com.laytonsmith.core.events.EventUtils;
 
-import me.macjuul.chexodius.CHExodiusMain;
+import me.macjuul.chexodius.LifeCycle;
 
 @SuppressWarnings("deprecation")
 public class EventListeners implements Listener {
@@ -22,11 +21,10 @@ public class EventListeners implements Listener {
 		if(listener == null){
 			listener = new EventListeners();
 		}
-		CHExodiusMain.chp.registerEvents(listener);
+		LifeCycle.chp.registerEvents(listener);
 	}
 	
 	public static void unregister() {
-		EntityToggleGlideEvent.getHandlerList().unregister(listener);
 	}
 	
 	@event
@@ -34,18 +32,9 @@ public class EventListeners implements Listener {
 		EventUtils.TriggerListener(Driver.EXTENSION, "projectile_hit_block", event);
 	}
 	
-	@EventHandler
-	public void onGlide(EntityToggleGlideEvent event) {
-		if(event.getEntityType().equals(EntityType.PLAYER) && ((Player) event.getEntity()).isFlying()) {
-			return;
-		}
-		ExodiusEvents.ExodiusEntityGlideEvent evt = new ExodiusEvents.ExodiusEntityGlideEvent(event);
-		EventUtils.TriggerListener(Driver.EXTENSION, "entity_toggle_glide", evt);
-	}
-	
-	@EventHandler
-	public void onSwap(PlayerSwapHandItemsEvent event) {
-		ExodiusEvents.ExodiusPlayerSwapHandItemsEvent evt = new ExodiusEvents.ExodiusPlayerSwapHandItemsEvent(event);
-		EventUtils.TriggerListener(Driver.EXTENSION, "player_swap_hand", evt);
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onEntityCombust(EntityCombustEvent event) {
+		BukkitMCEntityCombustEvent seme = new BukkitMCEntityCombustEvent(event);
+		EventUtils.TriggerListener(Driver.EXTENSION, "entity_combust", seme);
 	}
 }
